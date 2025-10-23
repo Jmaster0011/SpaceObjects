c8fd1afac3ed4b5cba24d3333cac2aa5fa031a2c
james <james@THE_JAM_101>
2025-10-23 15:36:31 -0500

    Add project files.

---
+++ IInstrument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceObjects
+{
+    // Interface for scientific instruments on probes 
+    public interface IInstrument
+    {
+        string InstrumentName { get; }
+        bool IsActive { get; set; }
+        string CollectData();
+        double GetBatteryLevel();
+        void RechargeBattery(double amount);
+    }
+}
